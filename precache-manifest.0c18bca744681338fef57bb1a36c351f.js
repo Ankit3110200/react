fd5ba72fbb3db5e4eceb2398343ae51c65c24874
Ankit3110200 <chotaliyaankit31@gmail.com>
2020-10-09 13:38:19 +0530

    Updates

---
+++ precache-manifest.0c18bca744681338fef57bb1a36c351f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb032ebf69f9282a76e32b921ed79840",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "91bd6e95c6ebaa853e86",
+    "url": "/react/static/css/main.231c7685.chunk.css"
+  },
+  {
+    "revision": "1844e09bba36b5a2b0d1",
+    "url": "/react/static/js/2.9dc20940.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.9dc20940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bd6e95c6ebaa853e86",
+    "url": "/react/static/js/main.efd1e3f7.chunk.js"
+  },
+  {
+    "revision": "94e420bcbc56461d34d9",
+    "url": "/react/static/js/runtime-main.b5c39aa2.js"
+  }
+]);
